5b66c749e8013369ea2cfbd977ed7fba26fc1654
Uliana <ulianav95v@gmail.com>
2023-04-17 17:56:20 +0300

    add language mutations

--- src/graphql/mutations/languageMutations.ts
+++ src/graphql/mutations/languageMutations.ts
@@ -1,1 +1,49 @@
 import { gql } from '@apollo/client';
+
+export const ADD_LANGUAGE = gql`
+  mutation addLanguage(
+    $language: String!
+    $info: String
+    $languageLevel: LanguageLevel
+    $resumeId: ID!
+  ) {
+    addLanguage(
+      language: $language
+      info: $info
+      languageLevel: $languageLevel
+      resumeId: $resumeId
+    ) {
+      language
+      info
+      languageLevel
+    }
+  }
+`;
+
+export const UPDATE_LANGUAGE = gql`
+  mutation updateLanguage(
+    $id: ID!
+    $language: String
+    $info: String
+    $languageLevel: LanguageLevelUpdate
+  ) {
+    updateLanguage(
+      id: $id
+      language: $language
+      info: $info
+      languageLevel: $languageLevel
+    ) {
+      language
+      info
+      languageLevel
+    }
+  }
+`;
+
+export const DELETE_LANGUAGE = gql`
+  mutation deleteLanguage($id: ID!) {
+    deleteLanguage(id: $id) {
+      id
+    }
+  }
+`;
