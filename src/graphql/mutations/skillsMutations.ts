45087a36d8eec51ac37c61a7f1e908cdd58a9dc2
Uliana <ulianav95v@gmail.com>
2023-04-17 17:58:13 +0300

    add skills mutations

--- src/graphql/mutations/skillsMutations.ts
+++ src/graphql/mutations/skillsMutations.ts
@@ -1,1 +1,44 @@
 import { gql } from '@apollo/client';
+
+export const ADD_SKILL = gql`
+  mutation addSkill(
+    $skill: String!
+    $info: String
+    $skillLevel: SkillLevel
+    $resumeId: ID!
+  ) {
+    addSkill(
+      skill: $skill
+      info: $info
+      skillLevel: $skillLevel
+      resumeId: $resumeId
+    ) {
+      skill
+      info
+      skillLevel
+    }
+  }
+`;
+
+export const UPDATE_SKILL = gql`
+  mutation updateSkill(
+    $id: ID!
+    $skill: String
+    $info: String
+    $skillLevel: SkillLevelUpdate
+  ) {
+    updateSkill(id: $id, skill: $skill, info: $info, skillLevel: $skillLevel) {
+      skill
+      info
+      skillLevel
+    }
+  }
+`;
+
+export const DELETE_SKILL = gql`
+  mutation deleteSkill($id: ID!) {
+    deleteSkill(id: $id) {
+      id
+    }
+  }
+`;
