3a9651f5dc6bf011fb19d1a87ed0f896948be506
Uliana <ulianav95v@gmail.com>
2023-08-28 10:52:13 +0300

    fix resumes presentation on main page

--- src/components/IndexPage/MyResumes/MyResumes.tsx
+++ src/components/IndexPage/MyResumes/MyResumes.tsx
@@ -3,18 +3,24 @@ import MyResumeItem from './MyResumeItem';
 import style from './MyResumes.module.scss';
 import { useTranslation } from 'next-i18next';
 import { ResumeDataSetter } from '@/providers/ResumeDataSetter/ResumeDataSetter';
+import { useEffect, useState } from 'react';
 
 type Props = {myResumes:IResume[]}
 
 const MyResumes = ({myResumes}: Props) => {
     const {t} = useTranslation()
-    const resumesGrid = Array.apply(null, Array(4*Math.ceil((myResumes.length===0 ? 4 : myResumes.length) / 4))).map((el, i) => myResumes[i])
+    const [resumes, setResumes] = useState<IResume[]>([]);
+
+    useEffect(() => {
+        const resumesGrid = Array.apply(null, Array(4*Math.ceil((myResumes.length===0 ? 4 : myResumes.length) / 4))).map((el, i) => myResumes[i]) 
+        setResumes(resumesGrid)
+    }, [myResumes])
 
     return (
         <div className={style.myResumes}>
             <h1>{t('my-resumes')}</h1>
             <div className={style.resumesGrid}>
-                {resumesGrid.map((resume:IResume, i: number) => (
+                {resumes.map((resume:IResume, i: number) => (
                     resume ? 
                         <ResumeDataSetter key={i} resumeData={resume}>
                             <MyResumeItem resume={resume}/>
